97d5fabe2f4c8dfc776cb2e5a7f9790593a0387a
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-05-19 19:02:21 +0500

    Creating Custom Class in Python; This commit depicts the Blank custom Class

---
+++ CustomClass-in-Python.py
@@ -0,0 +1,8 @@
+# Creating a Blank class
+class User:
+    # 'pass' keyword is used if there is nothing inside class or when class is expecting an indent
+    pass
+
+
+# constructing object for class
+my_user = User
