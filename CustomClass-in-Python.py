e1eabea8312d19ce19f0b77e837c3d361d397f24
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-05-19 22:37:33 +0500

    Creating Attribute for a Class

--- CustomClass-in-Python.py
+++ CustomClass-in-Python.py
@@ -5,4 +5,15 @@ class User:
 
 
 # constructing object for class
-my_user = User
+my_user1 = User
+my_user1.id = 34
+my_user1.username = "jawadkhanpk"
+
+print(f"{my_user1.id}\n{my_user1.username}")
+
+
+my_user2 = User
+my_user2.userid= "56"
+my_user2.username = "devadan"
+
+print(f"{my_user2.id}\n{my_user2.username}")
